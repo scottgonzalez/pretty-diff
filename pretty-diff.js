b2380a7e471c1f0218d0465316e0b50bd6211f8e
Scott González <scott.gonzalez@gmail.com>
2013-03-05 14:40:32 -0500

    Whitespace.

--- pretty-diff.js
+++ pretty-diff.js
@@ -9,26 +9,26 @@ diff( process.argv.slice( 2 ).join( " " ), function( error, parsedDiff ) {
 		process.stderr.write( error );
 		return;
 	}
-	
+
 	if ( !parsedDiff ) {
 		console.log( "No differences" );
 		return;
 	}
-	
+
 	generatePrettyDiff( parsedDiff );
 });
 
 function generatePrettyDiff( parsedDiff ) {
 	var template = fs.readFileSync( __dirname + "/template.html", "utf8" ),
 		diffHtml = "";
-		
+
 		for ( var file in parsedDiff ) {
 			diffHtml += "<h2>" + file + "</h2>" +
 			"<div class='file-diff'><div>" +
 				markUpDiff( parsedDiff[ file ] ) +
 			"</div></div>";
 		}
-		
+
 		fs.writeFileSync( "/tmp/diff.html", template.replace( "{{diff}}", diffHtml ) );
 		exec( "open /tmp/diff.html" );
 }
