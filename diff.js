584657e005b8ad94ca81bbfaa75e88cfb67784b3
Brendan Blackwood <elderz@gmail.com>
2013-12-16 17:55:12 -0500

    add line numbers

--- diff.js
+++ diff.js
@@ -27,15 +27,42 @@ module.exports = function( args, fn ) {
 
 function splitByFile( diff ) {
 	var filename,
-		files = {};
+		files = {},
+		matches,
+		oldLineNum,
+		newLineNum;
 
 	diff.split( "\n" ).forEach(function( line, i ) {
 		if ( line.charAt( 0 ) === "d" ) {
 			filename = line.replace( /^diff --git a\/(\S+).*$/, "$1" );
 			files[ filename ] = [];
+			oldLineNum = null;
+		} else if ( matches = line.match( /@@\ -[0-9]+(,[0-9]+)?\ \+([0-9]+)(,[0-9]+)?\ @@.*/ ) ) {
+			oldLineNum = newLineNum = matches[ 2 ];
+			files[ filename ].push({
+				line: line,
+				oldLineNum: null,
+				newLineNum: null
+			});
+		} else if ( matches = line.match( /^($esc\[[0-9;]+m)*([\ +-])/ ) ) {
+			files[ filename ].push({
+				line: line,
+				oldLineNum: oldLineNum,
+				newLineNum: newLineNum
+			});
+			if ( matches[ 2 ] != '-' ) {
+				newLineNum++;
+			} 
+			if ( matches[ 2 ] != '+' ) {
+				oldLineNum++;
+			}
+		} else {
+			files[ filename ].push({
+				line: line,
+				oldLineNum: null,
+				newLineNum: null
+			});
 		}
-
-		files[ filename ].push( line );
 	});
 
 	return files;
